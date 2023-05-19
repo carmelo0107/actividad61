47137a2656870e233da1543219f359aa6dfb86d7
carmelo0107 <132547364+carmelo0107@users.noreply.github.com>
2023-05-19 12:40:17 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+basic.showString("hola")
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
 basic.forever(function () {
 	
 })
